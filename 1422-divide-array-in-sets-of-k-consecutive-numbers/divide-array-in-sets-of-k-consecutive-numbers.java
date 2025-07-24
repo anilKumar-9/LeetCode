00a57adad6d28c6043b5b4913b9ca07d3e663157
Anil Kumar <127287203+anilKumar-9@users.noreply.github.com>
2025-07-24 14:24:52 +0530

    Time: 62 ms (77.45%) | Memory: 62.3 MB (17.53%) - LeetSync

--- 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPossibleDivide(int[] nums, int k) {
         Arrays.sort(nums);
-        Map<Integer,Integer> map=new TreeMap<>();
+        Map<Integer,Integer> map=new HashMap<>();
         int n=nums.length;
         for(int i=0;i<n;i++)
         {
