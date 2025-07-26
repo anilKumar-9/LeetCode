1363032652784ea667216068a43c7750ab2f3d28
Anil Kumar <127287203+anilKumar-9@users.noreply.github.com>
2025-07-26 10:06:24 +0530

    Time: 2 ms (98.60%) | Memory: 44.6 MB (80.42%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.java
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.java
@@ -1,39 +1,27 @@
 class Solution {
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
-        int i=0,j=nums.length-1;
+        int i=0,j=0,n=nums.length;
         ArrayList<Integer> list=new ArrayList<>();
-        while(i<=j)
+        while(i<n&&j<n)
         {
-            if(i==j &&nums[i]==key)
+            if(nums[j]!=key)
+            {
+                j++;
+            }
+            else if(i<j-k)
+            {
+                i++;
+            }
+            else if(i<=j+k)
             {
                 list.add(i);
                 i++;
-                break;
             }
-            if(nums[i]==key)
+            else
             {
-                list.add(i);
-            }
-             if(nums[j]==key)
-            {
-                list.add(j);
-            }
-            i++;
-            j--;
-        }
-        Set<Integer> set=new HashSet<>();
-        for( i=0;i<list.size();i++)
-        {
-            for( j=0;j<nums.length;j++)
-            {
-                if(Math.abs(j-list.get(i))<=k)
-                {
-                    set.add(j);
-                }
+                j++;
             }
         }
-        ArrayList<Integer> ans=new ArrayList<>(set);
-        Collections.sort(ans);
-        return ans;
+        return list;
     }
 }
