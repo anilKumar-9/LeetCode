357b4ecbd5a3689e965bf06b47d34fa3848495af
Anil Kumar <127287203+anilKumar-9@users.noreply.github.com>
2025-07-27 10:50:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (81.97%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -1,32 +1,24 @@
 class Solution {
     public int countHillValley(int[] nums) {
-        ArrayList<Integer> list=new ArrayList<>();
-        int n=nums.length;
-        list.add(nums[0]);
-        for(int i=1;i<n;i++)
-        {
-            if(nums[i]!=list.get(list.size()-1))
-            {
-                list.add(nums[i]);
+       int c = 0;
+        for(int i=1;i<nums.length-1;i++) {
+            if(nums[i] == nums[i-1]) continue;
+            int val = nums[i];
+            int left = i-1;
+            int right = i+1;
+            while(left >= 0 && val == nums[left]) {
+                left--;
+            }
+            while(right < nums.length && val == nums[right]) {
+                right++;
+            }
+            
+            if(left >= 0 && right < nums.length) {
+                if(val > nums[left] && val > nums[right]||val < nums[left] && val < nums[right]) {
+                    c++;
+                } 
             }
         }
-        int size=list.size();
-        if(size<3)
-        {
-            return 0;
-        }
-        int count=0;
-        int i=1,j=0,k=2;
-        while(k<size)
-        {
-            if(list.get(i)<list.get(j)&&list.get(i)<list.get(k)||list.get(i)>list.get(j)&&list.get(i)>list.get(k))
-            {
-                count++;
-            }
-            i++;
-            j++;
-            k++;
-        }
-        return count;
+        return c;
     }
 }
