177a19183e79678d3d2253fd411e0b34c8050ffd
Anil Kumar <127287203+anilKumar-9@users.noreply.github.com>
2025-07-29 12:56:11 +0530

    Time: 4 ms (99.52%) | Memory: 45.7 MB (24.98%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+       
+       int n=s.length(),i=0,j=s.length()-1;
+       while(i<=j)
+       {
+        if(s.charAt(i)!=s.charAt(j))
+        {
+            return palindrome(s,i+1,j)||palindrome(s,i,j-1);
+        }
+        i++;
+        j--;
+       }
+       return true;
+    }
+    static boolean palindrome(String s,int start,int end)
+    {
+        while(start<=end)
+        {
+            if(s.charAt(start++)!=s.charAt(end--))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
