35836fc64a7c94391a250231dbe2f5e8fa2ac738
Anil Kumar <127287203+anilKumar-9@users.noreply.github.com>
2025-08-01 23:07:14 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (8.62%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,48 +1,27 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-         ArrayList<ArrayList<Integer>> adjList=convertMatToList(isConnected);
-         int V=adjList.size();
+         int V=isConnected.length;
          boolean []vis=new boolean[V];
          int count=0;
          for(int i=0;i<V;i++)
          {
             if(!vis[i])
             {
-                dfs(i,adjList,vis);
+                dfs(i,isConnected,vis);
                 count++;
             }
          }
          return count;
     }
-    static ArrayList<ArrayList<Integer>> convertMatToList(int [][] mat)
-    {
-        ArrayList<ArrayList<Integer>> adjList=new ArrayList<>();
-        for(int i=0;i<mat.length;i++)
-        {
-            adjList.add(new ArrayList<>());
-        }
-        for(int i=0;i<mat.length;i++)
-        {
-            for(int j=0;j<mat[0].length;j++)
-            {
-                if(i!=j&&mat[i][j]==1)
-                {
-                    adjList.get(i).add(j);
-                    adjList.get(j).add(i);
-                }
-            }
-        }
-        return adjList;
-    }
-
-    static void dfs(int node,ArrayList<ArrayList<Integer>> adjList,boolean []vis)
+    
+    static void dfs(int node,int[][] isConnected ,boolean []vis)
     {
         vis[node]=true;
-        for(int i: adjList.get(node))
+        for(int i=0;i<isConnected.length;i++)
         {
-            if(!vis[i])
+            if(!vis[i]&&isConnected[node][i]==1)
             {
-                dfs(i,adjList,vis);
+                dfs(i,isConnected,vis);
             }
         }
     }
