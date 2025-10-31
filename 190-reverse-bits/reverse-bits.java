a378cb42471f99cab819f6c637970029a07d8b2f
Anil Kumar <127287203+anilKumar-9@users.noreply.github.com>
2025-10-31 07:51:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (14.82%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverseBits(int n) {
+        int ans=0;
+
+        for(int i=0;i<32;i++)
+        {
+            ans=ans<<1;
+            if((n&1)!=0)
+            {
+                ans=ans|1;
+            }
+            n>>=1;
+        }
+        return ans;
+    }
+}
