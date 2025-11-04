86d7ea1a6e7a742248c736fb156bc94b5c90f0f0
Anil Kumar <127287203+anilKumar-9@users.noreply.github.com>
2025-11-04 16:26:59 +0530

    Time: 15 ms (6.31%) | Memory: 52.4 MB (5.60%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> set=new HashSet<>();
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if(board[i][j]!='.')
+                {
+                    String b = "(" + board[i][j] + ")";
+                    if(!set.add(b+i)||
+                    !set.add(j+b)||
+                    !set.add((i/3)+b+(j/3)))
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+        }
+        return true;
+    }
+}
