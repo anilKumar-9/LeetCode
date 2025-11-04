e985432630b0efa241a863083419cbd8b4ab6af0
Anil Kumar <127287203+anilKumar-9@users.noreply.github.com>
2025-11-04 16:54:37 +0530

    Time: 1 ms (99.83%) | Memory: 46.1 MB (5.60%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,22 +1,41 @@
 class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        HashSet<String> set=new HashSet<>();
-        for(int i=0;i<9;i++)
-        {
-            for(int j=0;j<9;j++)
-            {
-                if(board[i][j]!='.')
-                {
-                    String b = "(" + board[i][j] + ")";
-                    if(!set.add(b+i)||
-                    !set.add(j+b)||
-                    !set.add((i/3)+b+(j/3)))
-                    {
-                        return false;
-                    }
-                }
+    public boolean isValidSudoku(char[][] mat) {
+        int[][] rows = new int[10][10];
+        int[][] cols = new int[10][10];
+        int[][] subMat = new int[10][10];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                // Skip empty cells
+                if (mat[i][j] == '.')
+                    continue;
+
+                // Current value
+                int val = mat[i][j]-'0';
+
+                // Check for duplicates in row
+                if (rows[i][val] == 1)
+                    return false;
+
+                // Mark as seen
+                rows[i][val] = 1;
+
+                // Check for duplicates in column
+                if (cols[j][val] == 1)
+                    return false;
+
+                // Mark as seen
+                cols[j][val] = 1;
+
+                // Check for duplicates in sub-grid
+                int idx = (i / 3) * 3 + j / 3;
+                if (subMat[idx][val] == 1)
+                    return false;
+
+                // Mark as seen
+                subMat[idx][val] = 1;
             }
-            
         }
         return true;
     }
