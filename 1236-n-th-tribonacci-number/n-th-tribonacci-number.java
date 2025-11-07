61715857dc058b8d5b43a0b888767a52c542fd84
Anil Kumar <127287203+anilKumar-9@users.noreply.github.com>
2025-11-07 16:06:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (6.65%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -9,14 +9,15 @@ class Solution {
             return 1;
         }
         int []dp=new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
+        int first=0,second=1,third=1;
 
         for(int i=3;i<=n;i++)
         {
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+            int fourth=first+second+third;
+            first=second;
+            second=third;
+            third=fourth;
         }
-        return dp[n];
+        return third;
     }
 }
