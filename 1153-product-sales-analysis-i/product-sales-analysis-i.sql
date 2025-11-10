d51add1fa7a182a945302ac561bdaec5aed06b35
Anil Kumar <127287203+anilKumar-9@users.noreply.github.com>
2025-11-10 10:38:33 +0530

    Time: 1173 ms (68.55%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,5 +1,5 @@
 -- Write your PostgreSQL query statement below
 
 SELECT p.product_name,s.year,s.price 
-FROM Sales s FULL OUTER JOIN Product p
+FROM Sales s LEFT JOIN Product p
 on s.product_id=p.product_id where s.product_id IS NOT NULL;
