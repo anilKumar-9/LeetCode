71af51b6c1c5457b3d4c3e50c4c2f0bcb21eef98
Anil Kumar <127287203+anilKumar-9@users.noreply.github.com>
2025-11-18 11:30:29 +0530

    Time: 35 ms (30.34%) | Memory: 60.5 MB (5.35%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,38 +1,48 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        int n=nums.length,low=0,high=0;
-        Set<List<Integer>> set=new HashSet<>();
-        
-        for(int i=0;i<n;i++)
+        int n=nums.length;
+        List<List<Integer>> outer=new ArrayList();
+        for(int i=0;i<n-2;i++)
         {
-            low=i+1;
-            high=n-1;
+            if(i>0&&nums[i]==nums[i-1])
+            {
+                continue;
+            }
+            int low=i+1;
+            int high=n-1;
             while(low<high)
             {
                 int sum=nums[i]+nums[low]+nums[high];
                 if(sum==0)
                 {
-                   int x=nums[low];
-                   set.add(new ArrayList<>(Arrays.asList(nums[i],nums[low],nums[high])));
-                 
-                   while(low<high&&x==nums[low])
-                   {
+                    List<Integer> list=new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[low]);
+                    list.add(nums[high]);
+                    outer.add(list);
+                    while(low<high&&nums[low]==nums[low+1])
+                    {
+                        low++;
+                    }
+                    while(low<high&&nums[high]==nums[high-1])
+                    {
+                        high--;
+                    }
                     low++;
-                   }
-                   
-                }
-                else if(sum>0)
-                {
                     high--;
+                }
+                else if(sum<0)
+                {
+                    low++;
                 }
                 else
                 {
-                    low++;
+                    high--;
                 }
             }
         }
-        List<List<Integer>> res=new ArrayList<>(set);
-        return res;
+        
+        return outer;
     }
 }
