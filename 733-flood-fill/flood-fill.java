d832738397ac498c24ec0a8a064d955870695600
Anil Kumar <127287203+anilKumar-9@users.noreply.github.com>
2025-12-21 23:17:30 +0530

    Time: 1 ms (43.80%) | Memory: 47 MB (16.18%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,36 +1,41 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
         int n=image.length;
         int m=image[0].length;
+
         if(image[sr][sc]==color)
         {
             return image;
         }
-        int [][] distance={{1,0},{0,-1},{0,1},{-1,0}};
+
         int val=image[sr][sc];
+        Queue<int []> queue= new LinkedList<>();
         image[sr][sc]=color;
-        Queue<int []> q=new LinkedList<>();
-        q.offer(new int[]{sr,sc});
-        while(!q.isEmpty())
+        queue.offer(new int[]{sr,sc});
+
+        int [][] distances={{1,0},{0,1},{-1,0},{0,-1}};
+
+        while(!queue.isEmpty())
         {
-            int size=q.size();
-            for(int i=0;i<size;i++)
+            int size=queue.size();
+
+            
+            int [] values=queue.poll();
+            for(int [] distance: distances)
             {
-                int []values=q.poll();
-                int row=values[0];
-                int col=values[1];
-                for(int []dist:distance)
+                int nr=values[0]+distance[0];
+                int nc=values[1]+distance[1];
+
+                if(nr>=0&&nr<n&&nc>=0&&nc<m&&image[nr][nc]==val)
                 {
-                    int nr=row+dist[0];
-                    int nc=col+dist[1];
-                    if(nr>=0&&nc>=0&&nr<n&&nc<m&&image[nr][nc]==val)
-                    {
-                        image[nr][nc]=color;
-                        q.offer(new int[]{nr,nc});
-                    }
+                    image[nr][nc]=color;
+                    queue.offer(new int[]{nr,nc});
                 }
             }
+
         }
+
         return image;
     }
 }
