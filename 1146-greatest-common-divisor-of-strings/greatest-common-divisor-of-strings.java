20721fbb0969e47e5bb7091c7d446feab8bcd959
Anil Kumar <127287203+anilKumar-9@users.noreply.github.com>
2026-02-05 10:17:13 +0530

    Time: 2 ms (15.64%) | Memory: 44 MB (14.11%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        int len1=str1.length(),len2=str2.length();
+        for(int i=Math.min(len1,len2);i>=1;i--)
+        {
+            if(isValidDivisor(str1,str2,i))
+            {
+                return str1.substring(0,i);
+            }
+        }
+        return "";
+    }
+
+    static boolean isValidDivisor(String str1, String str2, int i)
+    {
+       int len1=str1.length(),len2=str2.length();
+        if(len1%i!=0&&len2%i!=0)
+        {
+            return false;
+        }
+        String base=str1.substring(0,i);
+        
+        return str1.replace(base,"").isEmpty()&&
+                str2.replace(base,"").isEmpty();
+    }
+}
